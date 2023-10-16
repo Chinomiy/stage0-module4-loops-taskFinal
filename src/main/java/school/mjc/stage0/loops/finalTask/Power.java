55366f1322c7183205bb11cacf4a79f2580c466e
Chinomiy <maruishiwo@gmail.com>
2023-10-16 16:22:12 +0300

    v1

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int sum = numberToPrint;
+        if (power == 1) {
+            System.out.println(numberToPrint);
+            return;
+        }
+        for (int i = 1;i < power; i++) {
+            sum *= 10;
+        }
+        sum = (power % 2 == 0) ? Math.abs(sum) : sum;
+        System.out.println(sum);
     }
 
     public static void main(String[] args) {
